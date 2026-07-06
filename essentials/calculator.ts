6037b2ece696610fe10d602be85b00747e7c76e2
Bartosz Konior <95236029+bartoszkonior@users.noreply.github.com>
2026-07-06 13:48:51 +0200

    Add essentials project

---
+++ essentials/calculator.ts
@@ -0,0 +1,77 @@
+type InvestmentData = {
+  initialAmount: number;
+  annualContribution: number;
+  expectedReturn: number;
+  duration: number;
+};
+
+type InvestmentResult = {
+  year: string;
+  totalAmount: number;
+  totalContributions: number;
+  totalInterestEarned: number;
+};
+
+type CalculationResult = InvestmentResult[] | string;
+
+function calculateInvestment(data: InvestmentData): CalculationResult {
+  const { initialAmount, annualContribution, expectedReturn, duration } = data;
+
+  if (initialAmount < 0) return "Initial investment amount must be at least 0!";
+
+  if (duration <= 0) return "Provide valid amount of years";
+
+  if (expectedReturn < 0) return "Expected must be at least 0";
+
+  let total = initialAmount;
+  let totalContributions = 0;
+  let totalInterestEarned = 0;
+
+  const annualResults: InvestmentResult[] = [];
+
+  for (let i = 0; i < duration; i++) {
+    total = total * (1 + expectedReturn);
+    totalInterestEarned = total - totalContributions - initialAmount;
+    totalContributions = totalContributions + annualContribution;
+    total = total + annualContribution;
+
+    annualResults.push({
+      year: `Year ${i + 1}`,
+      totalAmount: total,
+      totalInterestEarned,
+      totalContributions,
+    });
+  }
+
+  return annualResults;
+}
+
+function printResults(results: CalculationResult) {
+  if (typeof results === "string") {
+    console.log(results);
+    return;
+  }
+
+  for (const yearEndResult of results) {
+    console.log(yearEndResult.year);
+    console.log(`Total: ${yearEndResult.totalAmount.toFixed(0)}`);
+    console.log(
+      `Total Contributions: ${yearEndResult.totalContributions.toFixed(0)}`
+    );
+    console.log(
+      `Total Interest Earned: ${yearEndResult.totalInterestEarned.toFixed(0)}`
+    );
+    console.log("=======================================");
+  }
+}
+
+const investmenData: InvestmentData = {
+  initialAmount: 5000,
+  annualContribution: 500,
+  expectedReturn: 0.08,
+  duration: 10,
+};
+
+const results = calculateInvestment(investmenData);
+
+printResults(results);
